d0a3585192c1856cf42bca164d38bf5220d9736e
Uili Fecteau <email": null>
2016-05-16 12:12:06 +1200

    page setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Knex = require('knex')
+var config = require('../knexfile')
+var knex = Knex(config[process.env.NODE_ENV || 'development'])
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Three Little Wigs' });
 });
+
+/*GET new Cumstomer*/
+
+
 
 module.exports = router;
