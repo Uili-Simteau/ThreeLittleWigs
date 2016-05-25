0283ea6b4b3a35047eff24498be597c418178ab4
Uili Fecteau <email": null>
2016-05-26 11:58:08 +1200

    clientside update

--- clientSide/index.js
+++ clientSide/index.js
@@ -1,7 +1,7 @@
 var request = require('superagent') // some request library
 var exampleTemplate = require('./views/example.js')
 
-request.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+request.get('/users', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
