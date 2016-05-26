f424bbef80b9879c1f663d3e95452fa62c1bbaf1
Uili Fecteau <email": null>
2016-05-26 15:33:47 +1200

    add a view and delete user.js

---
+++ clientSide/views/customerView.js
@@ -0,0 +1,32 @@
+var h = require('hyperscript')
+
+function exampleTemplate( object ) {
+  var name = object.name
+
+  var domElement =
+  h('div#page',
+    h('div#header',
+      h('h1.classy', "Welcome " + name + "!", { style: {'background-color': 'white'} })
+    ),
+    h('div#menu', { style: {'background-color': '#2f2'} },
+      h('ul',
+        h('li', 'one'),
+        h('li', 'two'),
+        h('li', 'three')
+      )
+    ),
+    h('h2', 'content title',  { style: {'background-color': '#f22'} }),
+    h('p',
+      "so it's just like a templating engine,\n",
+      "but easy to use inline with javascript\n"
+    ),
+    h('p',
+      "the intension is for this to be used to create\n",
+      "reusable, interactive html widgets. "
+    )
+  )
+
+  return domElement
+}
+
+module.exports = customerView
