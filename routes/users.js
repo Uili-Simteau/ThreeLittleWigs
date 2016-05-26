7d6a046a17d5897237709dd812324c7253891b62
Uili Fecteau <email": null>
2016-05-26 15:41:37 +1200

    delete user.js

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-cd missionsvar express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
