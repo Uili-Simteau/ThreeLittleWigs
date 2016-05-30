4b0dd2c1df02d30800724349da9de1c237b32507
Uili-Simteau <simteau@gmail.com>
2016-05-30 20:50:54 +1200

    trim tests of house tests

--- tests/uni-tests.js
+++ tests/uni-tests.js
@@ -42,7 +42,7 @@ test('setup'), function(t) {
   .then(function(){
     t.end()
   })
-})
+}
 
 
 /*add new customer test*/
@@ -53,60 +53,60 @@ test("test add customer function", function (t) {
   })
 })
 
-test('it gets all the houses', function (t) {
-  db.getAll('houses', function (err, resp) {
-    housesNames.map(function(house, i) {
+// test('it gets all the houses', function (t) {
+//   db.getAll('houses', function (err, resp) {
+//     housesNames.map(function(house, i) {
+//       //console.log(resp[i].name)
+//       t.equal(resp[i].name, house, 'Correct house names return in list')
+//     })
+//     t.end()
+//   })
+// })
+
+test('it gets all the customers', function (t) {
+  db.getAll('customers', function (err, resp) {
+    customers.map(function(house, i) {
       //console.log(resp[i].name)
-      t.equal(resp[i].name, house, 'Correct house names return in list')
+      t.equal(resp[i].name, house, 'Correct customer names return in list')
     })
     t.end()
   })
 })
 
-test('it gets all the characters', function (t) {
-  db.getAll('characters', function (err, resp) {
-    roaChars.map(function(house, i) {
-      //console.log(resp[i].name)
-      t.equal(resp[i].name, house, 'Correct charaters names return in list')
-    })
-    t.end()
-  })
-})
 
-
-test('it gets a particular House', function (t) {
+/*test('it gets a particular House', function (t) {
   db.findOne('houses', { id: 1 }, function (err, resp) {
     t.equal(resp["name"], "House Roa", 'it got the right house')
     t.end()
   })
-})
+})*/
 
-test('it gets a particular Character', function (t) {
-  db.findOne('characters', { id: 1 }, function (err, resp) {
+test('it gets a particular customer', function (t) {
+  db.findOne('customers', { id: 1 }, function (err, resp) {
     t.equal(resp.name, "Olly", 'it got the right house')
     t.end()
   })
 })
 
-test("test add House function", function (t) {
-  db.addNew('houses', Tieke, function(undefined, dbresponse) {
-    t.ok(dbresponse[0]>0)
-    t.end()
-  })
-})
+// test("test add House function", function (t) {
+//   db.addNew('houses', Tieke, function(undefined, dbresponse) {
+//     t.ok(dbresponse[0]>0)
+//     t.end()
+//   })
+// })
 
-test("test update House function", function(t){
-  db.update('houses',{id: TiekeUpdate.id},TiekeUpdate, function(undefined, dbresponse){
-  t.ok(dbresponse!==undefined)
-  t.end()
-  })
-})
+// test("test update House function", function(t){
+//   db.update('houses',{id: TiekeUpdate.id},TiekeUpdate, function(undefined, dbresponse){
+//   t.ok(dbresponse!==undefined)
+//   t.end()
+//   })
+// })
 
-test('it gets a particular Character', function (t) {
-  db.findAllInHouse('characters', { houseId: 1 }, function (err, resp) {
-    t.equal(resp.name, "Olly", 'it got the right house')
-    t.end()
-  })
-})
+// test('it gets a particular customer', function (t) {
+//   db.findAllInHouse('customers', { houseId: 1 }, function (err, resp) {
+//     t.equal(resp.name, "Olly", 'it got the right house')
+//     t.end()
+//   })
+// })
 
 
