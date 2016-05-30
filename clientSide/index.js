2569ab460a5c6be70293be103a5b316ab6b85232
Uili-Simteau <simteau@gmail.com>
2016-05-30 18:35:18 +1200

    don't use this until serverside is set

--- clientSide/index.js
+++ clientSide/index.js
@@ -1,21 +1,31 @@
-var request = require('superagent') // some request library
-var exampleTemplate = require('./views/example.js')
-var customerView = require('./views/customerView.js')
+// var request = require('superagent') // some request library
+// var exampleTemplate = require('./views/example.js')
+// var customerView = require('./views/customerView.js')
 
-document.onload(function() {
+import h from 'hyperscript'
+import $ from 'jquery'
+
+  // var uiliName =
+
+  //   h('div#page',
+  //     h('div#header',
+  //       h('h1.classU', "Uili Here"),
+  //       ));
+
+    $('main').append("uiliName")
   console.log("I'm ready!")
-});
 
-var button = document.getElementById(#button){
-  onclick('i am clicked')
-}
 
-request.get('/users', function(err, data) {
-  if (err) console.log(err) // do something
+// var button = document.getElementById(#button){
+//   onclick('i am clicked')
+// }
 
-  console.log(data.body)
+// request.get('/users', function(err, data) {
+//   if (err) console.log(err) // do something
 
-  var content = customerView({name: "Uili"})
-  document.body.appendChild(content)
-})
+//   console.log(data.body)
+
+//   var content = customerView({name: "Uili"})
+//   document.body.appendChild(content)
+// })
 
